6f2c5e1e270b062e07daca9013ad2fbd9400e9fa
Asanbek09 <samudinua@gmail.com>
2025-03-16 14:55:48 +0600

    db.py -> Customer

--- factory/app/model/db.py
+++ factory/app/model/db.py
@@ -16,5 +16,33 @@ class Login(db.Model):
         self.password = password
         self.user_type = user_type
 
-def __repr__(self):
-    return f"<Login {self.id} {self.username} {self.password} {self.user_type}>"
+    def __repr__(self):
+        return f"<Login {self.id} {self.username} {self.password} {self.user_type}>"
+    
+class Customer(db.Model):
+    __tablename__ = 'customer'
+    id = id.Column(db.Integer, db.ForeignKey('login.id'), primary_key = True)
+    firstname = db.Column(db.String(45))
+    lastname = db.Column(db.String(45))
+    middlename = db.Column(db.String(45))
+    email = db.Column(db.String(45))
+    mobile = db.Column(db.String(20))
+    address = db.Column(db.String(100))
+    status = db.Column(db.String(45))
+
+    login = db.relationship('Login', back_populates="customer")
+    orders = db.relationship('Orders', back_populates="customer")
+    shippings = db.relationship('Shipping', back_populates="customer")
+
+    def __init__(self, id, firstname, lastname, middlename, email, mobile, address, status):
+        self.id = id
+        self.firstname = firstname
+        self.lastname = lastname
+        self.middlename = middlename
+        self.email = email
+        self.mobile = mobile
+        self.address = address
+        self.status = status
+
+    def __repr__(self):
+        return f"<Customer {self.id} {self.firstname} {self.lastname} {self.middlename}>"
