4956d7acdc3795747c2155538589f61e536c74db
Asanbek09 <samudinua@gmail.com>
2025-03-17 13:36:40 +0600

    app/repository/customer.py

---
+++ factory/app/repository/customer.py
@@ -0,0 +1,48 @@
+from typing import Dict, Any, List
+from app.model.db import Customer
+from flask import current_app
+
+class CustomerRepository:
+    def __init__(self, db):
+        self.db = db
+        current_app.logger.info('CustomerRepository instance created')
+
+    def insert(self, cust:Customer) -> bool:
+        try:
+            self.db.session.add(cust)
+            self.db.sessio.commit()
+            current_app.logger.info('CustomerRepository inserted record')
+            return True
+        except Exception as e:
+            current_app.logger.info(f'CustomerRepository insert error: {e}')
+        return False
+    
+    def update(self, id:int, details:Dict[str, Any]) -> bool:
+        try:
+            self.db.session.query(Customer).filter(Customer.id == id).update(details)
+            self.db.session.commit()
+            current_app.logger.info('CustomerRepository updated record')
+            return True
+        except Exception as e:
+            current_app.logger.info(f'CustomerRepository update error: {e}')
+        return False
+    
+    def delete(self, id:int) -> bool:
+        try:
+            self.db.session.query(Customer).filter(Customer.id == id).delete()
+            self.db.session.commit()
+            current_app.logger.info('CustomerRepository deleted record')
+            return True
+        except Exception as e:
+            current_app.logger.info(f'CustomerRepository delete error: {e}')
+        return False
+    
+    def select_all(self) -> List[Any]:
+        users = self.db.session.query(Customer).all()
+        current_app.logger.info('CustomerRepository retrieved all record')
+        return users
+    
+    def select_one(self, id:int) -> Any:
+        users = self.db.session.query(Customer).filter(Customer.id == id).one_or_none()
+        current_app.logger.info('CustomerRepository retireved one record')
+        return users
