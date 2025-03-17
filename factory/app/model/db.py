ba9d794a2e5234d907ea3a8883140ea2f51edac0
Asanbek09 <samudinua@gmail.com>
2025-03-17 10:43:55 +0600

    db.py -> Payment + Shipping

--- factory/app/model/db.py
+++ factory/app/model/db.py
@@ -122,3 +122,54 @@ class PaymentType(db.Model):
 
     def __repr__(self):
         return f"<PaymentType {self.id} {self.name}>"
+    
+class Payment(db.Model):
+    __tablename__ = "payment"
+    id = db.Column(db.Integer, db.Sequence('payment_id_seq', increment=1), primary_key = True)
+    order_no = db.Column(db.String, db.ForeignKey('orders.order_no'), nullable = False)
+    ref_no = db.Column(db.String, nullable = False, unique = True)  
+    mode_payment = db.Column(db.Integer, db.ForeignKey('payment_type.id'),  nullable = False)  
+    amount = db.Column(db.Float, nullable = False)  
+    date_payment = db.Column(db.Date, nullable = False)  
+   
+    order = db.relationship('Orders', back_populates="payment")
+    payment_types = db.relationship('PaymentType', back_populates="payment")
+    shipping  = db.relationship('Shipping', back_populates="payment", uselist=False)
+   
+    def __init__(self, order_no, ref_no, mode_payment, amount, date_payment, id = None):
+        self.id = id
+        self.order_no = order_no
+        self.ref_no = ref_no
+        self.mode_payment = mode_payment
+        self.amount = amount
+        self.date_payment = date_payment
+      
+    def __repr__(self):
+        return f"<Payment {self.id} {self.order_no} {self.ref_no} {self.date_payment}>"
+
+class Shipping(db.Model):
+    __tablename__ = 'shipping'
+    id = db.Column(db.Integer, db.Sequence('shipping_id_seq', increment=1), primary_key = True)
+    cid = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable = False)  
+    pay_id = db.Column(db.Integer, db.ForeignKey('payment.id'), nullable = False)  
+    did = db.Column(db.Integer, db.ForeignKey('delivery_officer.id'), nullable = False)  
+    amount = db.Column(db.Float, nullable = False) 
+    status = db.Column(db.String, nullable = False)
+    date_shipping = db.Column(db.Date, nullable = False)  
+   
+    payment = db.relationship('Payment', back_populates="shipping")
+    shipping_details = db.relationship('ShippingDetails', back_populates="shipping", uselist=False)
+    delivery_officer = db.relationship('DeliveryOfficer', back_populates="shipping", uselist=False)
+    customer = db.relationship('Customer', back_populates="shippings")
+   
+    def __init__(self, cid, pay_id, did, amount, status, date_shipping, id = None):
+        self.id = id
+        self.cid = cid
+        self.did = did
+        self.pay_id = pay_id
+        self.amount = amount
+        self.status = status
+        self.date_shipping = date_shipping
+   
+    def __repr__(self):
+        return f"<Shipping {self.id} {self.pay_id} {self.cid} {self.did} {self.date_shipping} {self.amount}>"
