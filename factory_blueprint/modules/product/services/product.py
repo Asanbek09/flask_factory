4e4f973072159e5f8a59e8fcb2626527efa9027c
Asanbek09 <samudinua@gmail.com>
2025-03-19 15:59:33 +0600

    modules/product/services/product.py + resources -> css/js

---
+++ factory_blueprint/modules/product/services/product.py
@@ -0,0 +1,7 @@
+from modules.product.repository.product import ProductRepository
+
+def get_all_pid(db):
+    repo = ProductRepository(db)
+    recs = repo.select_all()
+    ids = [(p.id, p.name) for p in recs]
+    return ids
